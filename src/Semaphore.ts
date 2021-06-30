4377c0d6646bdfa572ad0224784ff9515cf138d3
Joost Lubach <joostlubach@gmail.com>
2021-06-30 12:01:11 +0200

    Added support for timeouts.

--- src/Semaphore.ts
+++ src/Semaphore.ts
@@ -1,40 +1,80 @@
 export interface Options {
   signalled?: boolean
   autoReset?: boolean
+  timeout?:   number
 }
 
 export default class Semaphore {
 
   constructor(
     private readonly options: Options = {}
-  ) {}
+  ) {
+    if (this.options.signalled) {
+      this.status = 'signalled'
+    } else {
+      this.status = 'pending'
+      this.reset()
+    }
+  }
 
-  private signalled: boolean = this.options.signalled ?? false
-
-  private resolves: Array<() => any> = []
+  private status: SemaphoreStatus
+  private resolves: SemaphoreResolve[] = []
 
   public reset() {
-    this.signalled = false
+    this.status = 'pending'
+    this.setTimeout()
   }
 
   public signal() {
-    this.signalled = true
-    this.resolves.forEach(resolve => resolve())
+    this.resolveWith('ok')
+  }
+
+  private resolveWith(result: 'ok' | 'timeout') {
+    this.status   = result === 'ok' ? 'signalled' : 'timeout'
+
+    this.resolves.forEach(it => it(result))
     this.resolves = []
 
+    this.clearTimeout()
     if (this.options.autoReset) {
       this.reset()
     }
   }
 
+  //------
+  // Timeout
+
+  private timeout: number | null = null
+
+  private setTimeout() {
+    const delay = this.options.timeout
+    if (delay == null) { return }
+
+    this.clearTimeout()
+    this.timeout = window.setTimeout(() => {
+      this.resolveWith('timeout')
+    }, delay)
+  }
+
+  private clearTimeout() {
+    if (this.timeout == null) { return }
+    window.clearTimeout(this.timeout)
+    this.timeout = null
+  }
+
+  //------
   // Promise interface
 
-  public then(resolve: () => any) {
-    if (this.signalled) {
-      resolve()
+  public then(resolve: SemaphoreResolve) {
+    if (this.status !== 'pending') {
+      resolve(this.status === 'signalled' ? 'ok' : 'timeout')
     } else {
       this.resolves.push(resolve)
     }
   }
 
 }
+
+export type SemaphoreStatus = 'pending' | 'signalled' | 'timeout'
+export type SemaphoreResolve = (result: 'ok' | 'timeout') => any
+export class SemaphoreTimeout extends Error {}
